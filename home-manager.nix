1c82b774622ddc63abae496f50687195a3ff67e5
Siddharth More <sidmoreoss@gmail.com>
2023-06-22 00:26:43 +0530

    Initial config

---
+++ home-manager.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz";
+in
+{
+  imports = [
+    (import "${home-manager}/nixos")
+  ];
+
+  home-manager.users.siddharth = {
+    /* The home.stateVersion option does not have a default and must be set */
+    home.stateVersion = "18.09";
+    /* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
+
+    # Configure shell
+    programs.zsh = {
+      enable = true;
+      shellAliases = {
+        ll = "ls -l";
+        update = "sudo nixos-rebuild switch";
+      };
+      enableCompletion = true;
+      enableAutosuggestions = true;
+      enableSyntaxHighlighting = true;
+      autocd = true;
+      history = {
+        size = 10000;
+      };
+    };
+  };
+}
+
