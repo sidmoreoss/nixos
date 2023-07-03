5c8831e86b2efba627b37700fe03c51297c3e002
Siddharth More <sidmoreoss@gmail.com>
2023-07-03 18:57:46 +0530

    Silent boot with plymouth

--- modules/boot/config/default.nix
+++ modules/boot/config/default.nix
@@ -21,18 +21,17 @@
     };
 
     # Silent Boot
-    # https://wiki.archlinux.org/title/Silent_boot
-    # https://discourse.nixos.org/t/removing-persistent-boot-messages-for-a-silent-boot/14835/10
-    kernelParams = [
-      "quiet"
-      "splash"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
+    kernelParams = [ 
+      "quiet" 
+      "rd.systemd.show_status=false" 
+      "rd.udev.log_level=3" 
+      "udev.log_priority=3" 
+      "boot.shell_on_fail" 
     ];
-    consoleLogLevel = 3;
-    
-    # https://github.com/NixOS/nixpkgs/pull/108294
+
+    # Plymouth hides all logs from boot screen
+    plymouth.enable = true;
+    consoleLogLevel = 0;
     initrd.verbose = false;
   };
 }
