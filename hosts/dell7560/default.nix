cb77e8ecf73c9af3693a7d9421a8b13dec4349cc
Siddharth More <sidmoreoss@gmail.com>
2023-07-16 03:37:59 +0530

    Enable high performance mode on dell 7560

--- hosts/dell7560/default.nix
+++ hosts/dell7560/default.nix
@@ -1,8 +1,10 @@
-{ ... }:
+{ lib, ... }:
 
 {
   # Specific configuration for dell7560 should go below
   imports = [ 
     ./hardware-configuration.nix # Current system hardware config 
   ];
+
+  powerManagement.cpuFreqGovernor = lib.mkForce "performance";
 }
