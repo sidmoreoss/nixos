a501373da452191d016167490d7799071fb6850b
Siddharth More <sidmoreoss@gmail.com>
2023-07-16 12:41:51 +0530

    Disable rtkit due to crackling audio

--- modules/hardware/sound/default.nix
+++ modules/hardware/sound/default.nix
@@ -2,7 +2,7 @@
 
 {
   sound.enable = true;  # Enable ALSA sound
-  security.rtkit.enable = true;  # Realtime scheduling mode, causes clipping https://wiki.archlinux.org/title/PipeWire#Troubleshooting
+  # security.rtkit.enable = true;  # Realtime scheduling mode, causes clipping https://wiki.archlinux.org/title/PipeWire#Troubleshooting
   services.pipewire = {
     enable = true;  # Enable sound with pipewire.
     alsa.enable = true;
