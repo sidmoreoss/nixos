043ee9dd273d774f1675af229f96b2ff6b1ab35f
Siddharth More <sidmoreoss@gmail.com>
2023-07-22 21:36:09 +0530

    Using zen kernel

--- hosts/dell7560/default.nix
+++ hosts/dell7560/default.nix
@@ -6,7 +6,9 @@
     ./hardware-configuration.nix # Current system hardware config 
   ];
 
-  boot.kernelPackages = pkgs.linuxPackages-rt_latest;
-
+  # boot.kernelPackages = pkgs.linuxPackages-rt_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_latest_xen_dom0;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
+  
   powerManagement.cpuFreqGovernor = "performance";
 }
