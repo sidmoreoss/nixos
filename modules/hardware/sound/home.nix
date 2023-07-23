6d7ebe6e9b8b70b23e49e24efcaad4859dbefc8a
Siddharth More <sidmoreoss@gmail.com>
2023-07-23 23:34:06 +0530

    Use zen kernel and remove pipewire min-quantum

--- modules/hardware/sound/home.nix
+++ modules/hardware/sound/home.nix
@@ -6,8 +6,8 @@
     {
       context.properties = {
         # default.clock.rate = 48000;
-        default.clock.allowed-rates = [ 44100 48000 88200 96000 192000 384000 ];
-        default.clock.min-quantum = 1024;
+        default.clock.allowed-rates = [ 44100 48000 88200 96000 192000 384000 ]
+        # default.clock.min-quantum = 1024
       };
     }
     '';
