0635d0c2856490a496497c2f4136495350f40780
Siddharth More <sidmoreoss@gmail.com>
2023-08-02 19:54:04 +0530

    Moved resources to a repository

--- modules/systemPackages/packages/home.nix
+++ modules/systemPackages/packages/home.nix
@@ -1,8 +1,12 @@
-{ ... }:
-{
+{ pkgs, ... } : {
   home.file = {  # My scripts
     ".local/bin" = {
-      source = ../../../resources/scripts;
+      source = pkgs.fetchFromGitLab {
+        owner = "lazylinux";
+        repo = "programs";
+        rev = "main";
+        sha256 = "sha256-pn1dMjJM5cbTMtedl4NKTUuzT8WlGzl7Hn/wpj0FHt4=";
+      } + "/programs";
       recursive = true;
     };
   };
