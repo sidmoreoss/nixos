0914a717e19c133b584a47e92eece151751aecd2
Siddharth More <sidmoreoss@gmail.com>
2023-08-05 01:03:27 +0530

    Fixed non-executable programs

--- modules/systemPackages/packages/home.nix
+++ modules/systemPackages/packages/home.nix
@@ -1,13 +1,28 @@
-{ pkgs, ... } : {
-  home.file = {  # My scripts
-    ".local/bin" = {
-      source = pkgs.fetchFromGitLab {
-        owner = "lazylinux";
-        repo = "programs";
-        rev = "main";
-        sha256 = "sha256-pn1dMjJM5cbTMtedl4NKTUuzT8WlGzl7Hn/wpj0FHt4=";
-      } + "/programs";
-      recursive = true;
-    };
+{ pkgs, ... } : 
+let 
+  # Refer https://nixos.wiki/wiki/Packaging/Binaries for packaging binaries
+  myPrograms = pkgs.stdenv.mkDerivation {
+    pname = "myPrograms";
+    version="v0.1";
+    src = pkgs.fetchFromGitLab {
+      owner = "lazylinux";
+      repo = "programs";
+      rev = "main";
+      sha256 = "sha256-zc7Y9ThgUk/1acFpeDrldUU5aRcbXKNidIFKik4qtk8=";
+    } + "/programs";
+
+    nativeBuildInputs = [
+      pkgs.autoPatchelfHook
+    ];
+
+    sourceRoot = "./programs";
+
+    installPhase = ''
+      for program in $(ls $src); do
+        install -m755 -D $program $out/bin/$program
+      done
+    '';
   };
+in {
+  home.packages = [ myPrograms ];
 }
