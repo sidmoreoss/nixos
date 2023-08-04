0ea7f039bf068c1231a8c679e931512906e7c10f
Siddharth More <sidmoreoss@gmail.com>
2023-08-04 22:49:38 +0530

    Update wallpapers

--- modules/systemPackages/wallpapers/home.nix
+++ modules/systemPackages/wallpapers/home.nix
@@ -4,7 +4,7 @@
       owner = "sidmoreoss";
       repo = "wallpapers";
       rev = "main";
-      sha256 = "sha256-Hv8vrdNqojS5WI/hxYzfjurp2QumFXbiTa4/aCtT4d4=";
+      sha256 = "sha256-8S3pggRvXnRuS/a7DK3XbeTFJoNw9Lu5ojqXMqn2/Ew=";
     } + "/Wallpapers";
     recursive = true;
   };
