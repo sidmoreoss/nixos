1f5ff303fda8e890b900c1f25cee1d42f3b3c59a
Siddharth More <sidmoreoss@gmail.com>
2023-08-05 11:51:35 +0530

    Added release version to myPrograms

--- modules/systemPackages/packages/home.nix
+++ modules/systemPackages/packages/home.nix
@@ -3,11 +3,11 @@ let
   # Refer https://nixos.wiki/wiki/Packaging/Binaries for packaging binaries
   myPrograms = pkgs.stdenv.mkDerivation {
     pname = "myPrograms";
-    version="v0.1";
+    version = "1.0.0";
     src = pkgs.fetchFromGitLab {
       owner = "lazylinux";
       repo = "programs";
-      rev = "main";
+      rev = "1.0.0";
       sha256 = "sha256-zc7Y9ThgUk/1acFpeDrldUU5aRcbXKNidIFKik4qtk8=";
     } + "/programs";
 
@@ -18,7 +18,7 @@ let
     sourceRoot = "./programs";
 
     installPhase = ''
-      for program in $(ls $src); do
+      for program in *; do
         install -m755 -D $program $out/bin/$program
       done
     '';
