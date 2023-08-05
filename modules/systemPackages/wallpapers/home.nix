59509d48a523dafc5b14d892b1328462b4ae72d4
Siddharth More <sidmoreoss@gmail.com>
2023-08-05 11:54:49 +0530

    Added release version to wallpapers

--- modules/systemPackages/wallpapers/home.nix
+++ modules/systemPackages/wallpapers/home.nix
@@ -3,7 +3,7 @@
     source = pkgs.fetchFromGitLab {
       owner = "sidmoreoss";
       repo = "wallpapers";
-      rev = "main";
+      rev = "1.0.0";
       sha256 = "sha256-8S3pggRvXnRuS/a7DK3XbeTFJoNw9Lu5ojqXMqn2/Ew=";
     } + "/Wallpapers";
     recursive = true;
